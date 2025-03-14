b269de31a3116dd1e1cb82c01b33156eacbc45f5
Thanawat-P <kumanero0@gmail.com>
2025-03-14 22:22:11 +0700

    d o n e

--- src/components/DateReserve.tsx
+++ src/components/DateReserve.tsx
@@ -3,20 +3,12 @@
 import { DatePicker } from "@mui/x-date-pickers";
 import { LocalizationProvider } from "@mui/x-date-pickers/LocalizationProvider";
 import { AdapterDayjs } from "@mui/x-date-pickers/AdapterDayjs";
-import dayjs, { Dayjs } from "dayjs";
 
-interface DateReserveProps {
-  onDateChange: (date: Date | null) => void;
-}
 
-const DateReserve: React.FC<DateReserveProps> = ({ onDateChange }) => {
-  const handleChange = (date: Dayjs | null) => {
-    onDateChange(date ? date.toDate() : null); // Convert Dayjs to JS Date
-  };
-
+const DateReserve = () => {
   return (
     <LocalizationProvider dateAdapter={AdapterDayjs}>
-      <DatePicker className="bg-white" onChange={handleChange} />
+      <DatePicker className="bg-white"/>
     </LocalizationProvider>
   );
 };
